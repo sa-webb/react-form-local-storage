e2e90fede9f27f6d0eece4ec7471ec5ca00a4497
sa-webb <sawebb30@gmail.com>
2020-05-27 14:47:04 -0400

    Storage nullHandler

--- src/context.js
+++ src/context.js
@@ -1,20 +1,24 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useEffect } from 'react';
 
 let reducer = (info, newInfo) => {
   if (newInfo === null) {
-    localStorage.removeItem("info");
-    return initialState;
+    if (typeof Storage !== 'undefined') {
+      localStorage.removeItem('info');
+      return initialState;
+    } else {
+      return null;
+    }
   }
   return { ...info, ...newInfo };
 };
 
 const initialState = {
-  name: "John Smith",
+  name: 'John Smith',
   age: 0,
-  email: "john.smith@gmail.com"
+  email: 'john.smith@gmail.com',
 };
 
-const localState = JSON.parse(localStorage.getItem("info"));
+const localState = JSON.parse(localStorage.getItem('info'));
 
 const InfoContext = React.createContext();
 
@@ -22,7 +26,7 @@ function InfoProvider(props) {
   const [info, setInfo] = useReducer(reducer, localState || initialState);
 
   useEffect(() => {
-    localStorage.setItem("info", JSON.stringify(info));
+    localStorage.setItem('info', JSON.stringify(info));
   }, [info]);
 
   return (
